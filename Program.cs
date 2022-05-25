56dbd14d1b5f189894239b20a6f7770a0f4374c5
Your Name <you@example.com>
2022-05-25 17:30:59 -0300

    hola

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("CALCULADORA VERSION 1");
+short start = 0;
+do
+{
+    
+    Console.WriteLine("Seleccione la operación que desea realizar: ");
+    Console.WriteLine("1.Suma");
+    Console.WriteLine("2.Resta");
+    Console.WriteLine("3.Multiplicacion");
+    Console.WriteLine("4.Division");
+    int operador;
+    operador = Int32.Parse(Console.ReadLine());
+    int a,b;
+    Console.WriteLine("Ingrese el operando a: ");
+    a = Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el operando b: ");
+    b = Int32.Parse(Console.ReadLine());
+
+    int resultado;
+    switch (operador)
+    {
+        case 1:
+            resultado = Suma(a,b);
+            Console.WriteLine("El resultado de a+b es " + resultado);
+            break;
+        case 2:
+            resultado = Resta(a,b);
+            Console.WriteLine("El resultado de a+b es " + resultado);
+            break;
+        case 3:
+            resultado = Multiplicacion(a,b);
+            Console.WriteLine("El resultado de a+b es " + resultado);
+            break;
+        case 4:
+            resultado = Division(a,b);
+            Console.WriteLine("El resultado de a+b es " + resultado);
+            break;
+    }
+    Console.WriteLine("Desea realizar otra operacion? Seleccione 0.");
+    start = short.Parse(Console.ReadLine());
+} while (start==0);
+
+
+int Suma(int a, int b) {
+    return a+b;
+}
+int Resta(int a, int b) {
+    return a-b;
+}
+int Multiplicacion(int a, int b) {
+    return a*b;
+}
+int Division(int a, int b) {
+    while (b == 0)
+    {
+        Console.WriteLine("Ingrese el operando b distinto de cero: ");
+        b = Int32.Parse(Console.ReadLine());
+    }
+    return a/b;
+}
+
+
+Console.WriteLine("CALCULADORA VERSION 2");
+Console.WriteLine("Ingrese un numero: ");
+double num;
+num = double.Parse(Console.ReadLine());
+Console.WriteLine("Valor absoluto: " + Math.Abs(num));
+Console.WriteLine("El cuadrado: " + Math.Pow(num,2));
+Console.WriteLine("La raiz cuadrada: " + Math.Sqrt(num));
+Console.WriteLine("El seno: " + Math.Sin(num));
+Console.WriteLine("El coseno: " + Math.Cos(num));
+Console.WriteLine("La parte entera: " + Convert.ToInt32(num));
+
+double x,y;
+Console.WriteLine("Ingrese el primer numero: ");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+y = double.Parse(Console.ReadLine());
+Console.WriteLine("El máximo es " + Math.Max(x,y));
+Console.WriteLine("El minimo es " + Math.Min(x,y));
+
+
+
